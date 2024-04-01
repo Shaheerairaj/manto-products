bfc2063a9b5b1344db12c9aa49944d7cb85e2bc0
Shaheer <shaheerairajahmed@gmail.com>
2024-04-01 21:18:04 +0400

    initial commit

---
+++ web_scrapper.py
@@ -0,0 +1,13 @@
+import os
+import pandas as pd
+import requests
+import bs4
+import lxml
+import functions.web_scrapper_functions as scrpr
+import mysql.connector
+import json
+import datetime
+
+import logging
+import warnings
+warnings.filterwarnings("ignore")
